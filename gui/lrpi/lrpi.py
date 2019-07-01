6baf2ca1b8e56d30519d0dc738c9859fe52529b7
Jason Brewer <jason@urawizard.com>
2019-07-01 18:34:07 +0100

    #25 added some notes for the pywebview->JavaScript comm links

--- gui/lrpi/lrpi.py
+++ gui/lrpi/lrpi.py
@@ -2,6 +2,10 @@
 
 import webview, threading
 
+# Sending events from JS -> Python and back:
+# https://askubuntu.com/questions/97430/connect-webkit-webview-form-to-a-python-callback
+# See also: https://github.com/r0x0r/pywebview/blob/master/examples/todos/assets/script.js
+
 class Api():
     def event(self, param):
         print('select audio file!')
@@ -10,14 +14,10 @@ if __name__ == '__main__':
     # t = threading.Thread(target=load_html)
     # t.start()
 
-    print('Begin...')
+    print('App start...')
 
     api = Api()
     webview.config.gui = "qt"
     webview.create_window('Lushroom Pi', 'www/index.html',
                           js_api=api, width=400, height =600, min_size=(400, 600), confirm_quit=True,
                           text_select=True, resizable=False, debug=True)
-
-    # Sending events from JS -> Python and back:
-    # https://askubuntu.com/questions/97430/connect-webkit-webview-form-to-a-python-callback
-    # See also: https://github.com/r0x0r/pywebview/blob/master/examples/todos/assets/script.js
--- gui/lrpi/www/js/controllers.js
+++ gui/lrpi/www/js/controllers.js
@@ -18,11 +18,19 @@ function ($scope, $stateParams) {
     var vm = this;
     vm.selectAudioFile = selectAudioFile;
 
+    // For selecting files (from http://www.myegotraps.com/pywebview-2-0-released/):
+    // Other new features include:
+
+    // When creating a window, you can now specify a relative path to a html file 
+    // (that is without a file:// or any other protocol). This way relatively linked resources will be resolved correctly. This will work when application is frozen too.
+    // target='_blank' links are opened in an external browser
+    // Text selection on non input elements is disabled by default
+    // new load_css function for ahem loading custom css
+
     function selectAudioFile() {
         console.log('click!');
         window.pywebview.api.event()
     }
-
 
 }])
    
